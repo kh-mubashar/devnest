bd7ebf2090a74dac1c3820c1afc11ac1885220aa
Bash <mubasherkhawaja@gmail.com>
2025-05-20 05:48:40 +0100

    fix: update ci/cd pipeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to DevNest 🐣</h1>
+      <h1 className="text-4xl font-bold">
+        Welcome to DevNest 🐣 wit CI/CD pipleine work
+      </h1>
     </main>
   );
 }
